7cc797e085a4b3b0ac2d1c42f96a9fc177006b9d
Natsyshirama <fandresenajoy@gmail.com>
2025-05-21 13:08:00 +0300

    get allClient

---
+++ app/Services/ClientAPI.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
+
+
+class ClientAPI{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://erpnext.localhost:8000';
+    }
+    
+    public function getAllClient(){
+        $sid = Session::get('sid');
+
+        if (!$sid) {
+            throw new \Exception('Non connecté');
+        }
+        $reponse =Http::withHeaders([
+            'Cookie' => 'sid=' . $sid
+        ])->get($this->baseUrl . '/apii/resource/Customer',[
+            'fields' => json_encode(['name', 'cstomer_group', 'territory'])
+        ]);
+        if (!$reponse->successful()) {
+            throw new \Exception("Erreur API Fournisseur : " . $reponse->body());
+        }
+        return $reponse->json('data');
+
+    }
+}

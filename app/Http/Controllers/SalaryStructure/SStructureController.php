51f8ceb4bd30fe6aabad8e0fbbc0ad4bf991d75f
Natsyshirama <fandresenajoy@gmail.com>
2025-06-03 22:06:55 +0300

    controller index ,show

---
+++ app/Http/Controllers/SalaryStructure/SStructureController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\SalaryStructure;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Services\SalaryStructur;
+
+class SStructureController extends Controller
+{
+    protected $sstructApi;
+
+    public function __construct(SalaryStructur $sstructApi)
+    {
+        $this->sstructApi = $sstructApi;
+    }
+
+    public function index(){
+        try{
+            $sstructures = $this->sstructApi->getSalaryStructure();
+
+            return view('salaryStructure.index',[
+                'sstructures' => $sstructures
+            ]);
+        } catch (\Exception $e) {
+            Log::error('Erreur lors de la récupération des listes Salary Structure', [
+                'error' => $e->getMessage(),
+                'code' => $e->getCode(),
+                'trace' => $e->getTraceAsString()
+            ]);
+            return redirect()->back()->withErrors(['message' => $e->getMessage()]);
+        }
+    }
+
+    public function show(Request $request){
+        $name = $request->input('name');
+        try{
+            $detail = $this->sstructApi->getSalaryStrDetails($name);
+            return view('salaryStructure.show',
+            [
+                'detail' => $detail
+            ]);
+
+         }catch (\Exception $e) {
+            Log::error('Erreur lors de la récupération details ', [
+                'error' => $e->getMessage(),
+                'code' => $e->getCode(),
+                'trace' => $e->getTraceAsString()
+            ]);
+            return redirect()->back()->withErrors(['message' => $e->getMessage()]);
+        }
+    }
+}
